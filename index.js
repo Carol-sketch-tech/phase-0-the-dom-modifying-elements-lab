e0a6f479822b3b14bcd8f3675f15d2312dbf0735
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-11 00:14:06 +0300

    re-read

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+const newNode= document.createElement('p')
+// append the new creatednode into the exsiting DOM 
+document.body.append('newNode')
+
+// remove elements from the DOM
+// removeChild()
+// we use removeChild() to remove a particular child an element
+someElement.removeChild(someChildElement);
+const ul=document.getElementsByTagName("ul")[0];
+const secondChild =ul.querySelector('li:nth-child(2)');
+ul.removeChild(secondChild);
+
+// element.remove()
+Element.remove();
+// example 
+ul.remove();
+
+
